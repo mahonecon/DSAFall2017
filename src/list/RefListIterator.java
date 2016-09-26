916d7f357f0260b51622189b4a294f2c70ee8ff6
Conkilla <mahonecon@gmail.com>
2016-09-26 09:48:08 -0400

    RefListIterator finished

--- src/list/RefListIterator.java
+++ src/list/RefListIterator.java
@@ -3,7 +3,7 @@ package list;
 /** A class that implements ListIterators for LinkedLists */
 public class RefListIterator<E> extends RefIterator<E> implements ListIterator<E> {
 	//cursor refers...
-	boolean forward = true;
+	private boolean forward = true;
 	
 	public RefListIterator(LinkedList<E> linkedList){
 		super(linkedList);
@@ -14,5 +14,42 @@ public class RefListIterator<E> extends RefIterator<E> implements ListIterator<E
 		for(int i = 0; i < start; i++) {
 			cursor = cursor.next;
 		}
+	}
+	
+	public boolean hasNext() {
+		if(list.isEmpty())
+			return false;
+		
+		if(forward) 
+			return cursor.next != list.tail;
+		else return true;
+	}
+	
+	public E next() {
+		if(forward)
+			cursor = cursor.next;
+		forward = true;
+		return cursor.value;
+	}
+	
+	public boolean hasPrevious() {
+		if(list.isEmpty()) 
+			return false;
+		if(!forward) 
+			return cursor.prev != list.head;
+		else return true;
+	}
+	
+	public E previous() {
+		if(!forward)
+			cursor = cursor.prev;
+		forward = false;
+		return cursor.value;
+	}
+	
+	public void remove() {
+		super.remove();
+		if(!forward)
+			cursor = cursor.next;
 	}
 }
