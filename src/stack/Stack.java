44dae1bf8fa4c882bb5b17fd85c6665fc779af67
Conkilla <mahonecon@gmail.com>
2016-09-26 10:16:44 -0400

    Added StackADT<E> interface & Stack<E> class.

---
+++ src/stack/Stack.java
@@ -0,0 +1,27 @@
+package stack;
+
+import list.*;
+
+/** A Stack can be implemented with some kind of List */
+public class Stack<E> implements StackADT<E> {
+
+	public Stack() {
+		
+	}
+	
+	public E push(E value) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public E pop() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public E peek() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
