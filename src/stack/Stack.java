f43253e9c8d1de8a7145a529eb992307003c9fea
Conkilla <mahonecon@gmail.com>
2016-09-26 10:32:41 -0400

    Minor edits to Stack<E> class.

--- src/stack/Stack.java
+++ src/stack/Stack.java
@@ -5,23 +5,26 @@ import list.*;
 /** A Stack can be implemented with some kind of List */
 public class Stack<E> implements StackADT<E> {
 
-	public Stack() {
-		
+	List<E> values = new ArrayList<E>();
+	
+	public Stack(boolean refBased) {
+		if(refBased) values = new LinkedList<E>();
+	}
+	
+	public Stack() {		
 	}
 	
 	public E push(E value) {
-		// TODO Auto-generated method stub
-		return null;
+		values.add(value);
+		return value;
 	}
 
 	public E pop() {
-		// TODO Auto-generated method stub
-		return null;
+		return values.remove(values.size() - 1);
 	}
 
 	public E peek() {
-		// TODO Auto-generated method stub
-		return null;
+		return values.get(values.size() - 1);
 	}
 
 }
