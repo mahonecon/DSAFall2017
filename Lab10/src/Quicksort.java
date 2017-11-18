83e4146e6736f7db3be50c2bedb40e1a833674da
CJ Mahoney <CJ Mahoney@Conor-GPC.fios-router.home>
2017-11-18 16:04:03 -0500

    Quicksort ver1 complete

--- Lab10/src/Quicksort.java
+++ Lab10/src/Quicksort.java
@@ -3,40 +3,41 @@ import java.util.Random;
 public class Quicksort {
 	private Random randy = new Random();
 	
+        //Breaks the array into partitions, and sorts the partitions
 	private int partition(int[] array, int min, int max) {
-		
-		int pivotIndex = randy.nextInt(max-min) + min;
+		int pivotIndex = randy.nextInt(max - min + 1) + min; //random pivot each time
 		int pivot = array[pivotIndex];
-		int a = pivotIndex;
-		int b = pivotIndex;
+		int a = min;
+                int b = max;
 		
-		for(int i = min; i < max; i++) {
-			if(array[i] <= pivot && a > 0) {
-				a--;
-				int temp = array[a];
-				array[a] = array[i];
-				array[i] = temp;
-			}
-			if(array[i] > pivot && b < max) {
-				b++;
-				int temp = array[b];
-				array[b] = array[i];
-				array[i] = temp;
-			}
-		}
-		
-		return pivotIndex;
-	}
+		while(a <= b) { //while the iterating and decrementing values are not equal to each other
+                    while(array[a] < pivot) { //if the item at array[a] is less than the pivot, increment a
+                        a++;
+                    }
+                    while(array[b] > pivot) { //if the item at array[b] is greater than the pivot, increment b
+                        b--;
+                    }
+                    if(a <= b) {
+                        //swap array[a] and array[b]
+                        int temp = array[a];
+                        array[a] = array[b];
+                        array[b] = temp;
+                        a++; //increment a
+                        b--; //decrement b
+                    }
+                }		
+		return a; //return a as the new index
+	}//end partition
 	
 	public int[] sort(int[] array, int min, int max) {
-		
-		if(min < max) {
-			
-			int pivot = partition(array, min, max);
-			
-			sort(array, min, pivot - 1);
-			sort(array, pivot + 1, max);
-		}
-		return array;
-	}
+            int index = partition(array, min, max);
+            
+            if(min < index - 1) { //If the minimum is below index - 1, send the array to be partitioned and sorted again
+                sort(array, min, index - 1);
+            }
+            if(index < max) { //If the maximum is greater than the index, send the array to be sorted again.
+                sort(array, index, max);
+            }
+            return array;
+	}//end sort
 }
