33b69329cd0213788e4c64b7ac6c01b30cb86bb6
mahoneyc8 <mahoneyc8@JameLab211326.rowanads.rowan.edu>
2017-11-28 18:32:26 -0500

    Attempted to make Data.java and Driver.java, kinda works.

--- Lab11/src/Driver.java
+++ Lab11/src/Driver.java
@@ -3,22 +3,25 @@ import java.io.*;
 public class Driver {
 
 	public static BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
-	
+	public static MyBinarySearchTreePlus<Data<String>, String> tree = new MyBinarySearchTreePlus();
+        public static String rootItem = "root"
+        public static Data r = new Data(rootItem);
+        
 	public static void main(String[] args) throws IOException {
 		
 		int code = 0;
         
         while(code != 10) {
         	System.out.println("Menu! Input number to reach menu option!");
-                System.out.println("\t" + "1.");
-                System.out.println("\t" + "2.");
-                System.out.println("\t" + "3.");
-                System.out.println("\t" + "4.");
-                System.out.println("\t" + "5.");
-                System.out.println("\t" + "6.");
-                System.out.println("\t" + "7.");
-                System.out.println("\t" + "8.");
-                System.out.println("\t" + "9.");
+                System.out.println("\t" + "1. Insert key in BST.");
+                System.out.println("\t" + "2. Delete key from BST.");
+                System.out.println("\t" + "3. Search for key in BST.");
+                System.out.println("\t" + "4. Display height of BST.");
+                System.out.println("\t" + "5. Display size of BST.");
+                System.out.println("\t" + "6. Display last item of Deq.");
+                System.out.println("\t" + "7. Display content of BST in order.");
+                System.out.println("\t" + "8. Display content of BST in postorder.");
+                System.out.println("\t" + "9. Build copy of the tree, and test it.");
                 System.out.println("\t" + "10. Exit.");
         	System.out.print("Make your menu selection now: ");
         	code = Integer.valueOf(stdin.readLine());
@@ -66,7 +69,12 @@ public class Driver {
         }//end while
 	}//end main
 	
-	public static void one() {}
+	public static void one() throws IOException {
+            Data temp;
+            System.out.println("Input key to insert");
+            temp = new Data<String>(stdin.readLine());
+            
+        }
 	public static void two() {}
 	public static void three() {}
 	public static void four() {}
