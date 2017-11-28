b2e7193100bb9c4f9b5fba95141c0b9fdb5d474b
mahoneyc8 <mahoneyc8@JameLab211326.rowanads.rowan.edu>
2017-11-28 18:32:51 -0500

    Attempted to make Data.java and Driver.java, kinda works.

---
+++ Lab11/src/Data.java
@@ -0,0 +1,11 @@
+
+public class Data <KT extends Comparable<? super KT>> extends KeyedItem<KT>{
+	private String data;
+	public Data(KT key) {
+		super(key);
+	}
+	public String toString(KT key){
+		return data;
+	}
+
+}
