9eb1bd0f35aa18107c106ca2ab6cdf6b38d24e04
CJ Mahoney <CJ Mahoney@Conor-GPC.fios-router.home>
2017-12-02 17:51:43 -0500

    Replaced ListArrayBasedPlus with Conor's

--- DSAProject/src/ListArrayBasedPlus.java
+++ DSAProject/src/ListArrayBasedPlus.java
@@ -1,56 +1,71 @@
-
-import java.util.Arrays;
-
 public class ListArrayBasedPlus<T> extends ListArrayBased<T> {
 
     public ListArrayBasedPlus() {
         super();
-
     }
 
-    public void add(int index, T item) throws IndexOutOfBoundsException {
-        if (super.numItems == super.items.length) {
-            resize();
-
+    public boolean isEmpty() {
+        if(size() == 0) {
+            return true;
         }
+        return false;
+    }
+
+    public int size() {
+        return super.size();
+    }
+
+    public void removeAll() {
+        super.removeAll();
+    }
+
+    public void add(int index, T item) {
         try {
             super.add(index, item);
-            System.out.println("Items " + item.toString() + " added to postion " + index);
-        } catch (IndexOutOfBoundsException ex) {
-            System.out.println("Postion is out of range");
+        } catch (ListIndexOutOfBoundsException x) {
+            resize();
+            super.add(index, item);
+        } catch (ListException x) {
+        	resize();
+        	super.add(index, item);
+        }
+    }
+
+    public T get(int index) {
+        return super.get(index);
+    }
+
+    public void remove(int index) {
+        super.remove(index);
+    }
+
+    public void reverse() {
+        T[] temp = (T[])new Object[size()];
+        if(size() >= 1) {
+            for(int i = 0; i < size(); i++) {
+                temp[i] = items[(size() - 1) - i];
+            }
+            items = temp;
+        }
+        else {
+            System.out.println("List has no items to reverse!");
         }
     }
 
     public void resize() {
-        int newLength = 0;
-        if (super.numItems % 2 == 0) {
-            newLength = (int) ((int) (super.numItems + 1) * 1.5);
-        } else {
-            newLength = (int) ((int) (super.numItems + 1.5) * 1.5);
+        int newSize = (int)Math.round(1.5*size());
+        T[] temp = (T[])new Object[newSize];
+        for(int i = 0; i < size(); i++) {
+            temp[i] = items[i];
         }
-        Object[] newArray = (T[]) new Object[newLength];
-        for (int k = 0; k < super.numItems; k++) {
-            newArray[k] = super.items[k];
-        }
-        super.items = (T[]) newArray;
-    }
-
-    public void reverse() {
-        int size = super.numItems;
-        int index = size - 1;
-        Object[] revArray = (T[]) new Object[size];
-        for (int r = 0; r < super.numItems; r++) {
-            revArray[r] = super.items[index - r];
-        }
-        super.items = (T[]) revArray;
+        items = temp;
     }
 
     public String toString() {
-        String string = "";
-        for (int s = 0; s < numItems; s++) {
-            string = string + items[s] + " ";
+        String s = "";
+        for(int i = 0; i < size(); i++) {
+            s += items[i].toString() + " ";
         }
-        return string;
+        return s;
     }
-
 }
