96d8475f73750689b17a0b0f8cb4a3d2d3cc2b17
CJ Mahoney <CJ Mahoney@Conor-GPC.fios-router.home>
2017-12-02 15:42:00 -0500

    Runway initialization added

--- DSAProject/src/Driver.java
+++ DSAProject/src/Driver.java
@@ -3,7 +3,7 @@ import java.io.*;
 public class Driver {
 
 	public static BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
-	public static Queue airport = new Queue();
+	public static Queue<Runway> airport = new Queue<Runway>();
 	public static ListArrayBasedPlus planes = new ListArrayBasedPlus();
 	
 	
@@ -13,18 +13,24 @@ public class Driver {
 		
 		System.out.println("Please enter the number of runways at the airport: ");
 		int numRunways = Integer.valueOf(stdin.readLine());
+                for(int i = 0; i < numRunways; i++) {
+                    System.out.print("Name of Runway " + (i+1) + ": ");
+                    String str = stdin.readLine();
+                    Runway run = new Runway(str);
+                    airport.enqueue(run);
+                }
         
-        while(code != 9) {
+            while(code != 9) {
         	System.out.println("Menu! Input number to reach menu option!");
-            System.out.println("\t" + "1. Plane enters the system.");
-            System.out.println("\t" + "2. Plane takes off.");
-            System.out.println("\t" + "3. Plane is allowed to re-enter a runway.");
-            System.out.println("\t" + "4. Runway opens.");
-            System.out.println("\t" + "5. Runway closes.");
-            System.out.println("\t" + "6. Display info about planes waiting to take off.");
-            System.out.println("\t" + "7. Display info about planes waiting to be allowed to re-enter a runway.");
-            System.out.println("\t" + "8. Display number of planes who have taken off.");
-            System.out.println("\t" + "9. Exit.");
+                System.out.println("\t" + "1. Plane enters the system.");
+                System.out.println("\t" + "2. Plane takes off.");
+                System.out.println("\t" + "3. Plane is allowed to re-enter a runway.");
+                System.out.println("\t" + "4. Runway opens.");
+                System.out.println("\t" + "5. Runway closes.");
+                System.out.println("\t" + "6. Display info about planes waiting to take off.");
+                System.out.println("\t" + "7. Display info about planes waiting to be allowed to re-enter a runway.");
+                System.out.println("\t" + "8. Display number of planes who have taken off.");
+                System.out.println("\t" + "9. Exit.");
         	System.out.print("Make your menu selection now: ");
         	code = Integer.valueOf(stdin.readLine());
         	System.out.print(code + "\n");
