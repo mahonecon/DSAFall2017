79cb0cf863d0d56b4e201cb74fc49dd3df06d0f4
Mahoney <mahoneyc8@elvix2.rowan.edu>
2017-12-04 21:28:47 -0500

    Shiet

--- Lab10/src/MergeSort.java
+++ Lab10/src/MergeSort.java
@@ -5,57 +5,57 @@
  * @author: Conor J. Mahoney
  */
 public class MergeSort {
-	private int[] array;
-	
-	public MergeSort() {
-	}//empty default constructor
-	
-        public int[] sort(int[] array) {
-            this.array = array;
-            for(int i = 1; i <= (array.length/2) + 2; i *= 2) {
-                for(int j = i; j < array.length; j += 2 * i) {
-                    merge(array, j-i, j, Math.min(j + i, array.length)); //Breaks the array into sub arrays and then re-merges them together in the correct ordered positions
-                }
+    private int[] array;
+
+    public MergeSort() {
+    }//empty default constructor
+
+    public int[] sort(int[] array) {
+        this.array = array;
+        for(int i = 1; i <= (array.length/2) + 2; i *= 2) {
+            for(int j = i; j < array.length; j += 2 * i) {
+                merge(array, j-i, j, Math.min(j + i, array.length)); //Breaks the array into sub arrays and then re-merges them together in the correct ordered positions
             }
-            return array;
-        }//end sort
-        
-        //Merges arrays together in ordered position.
-        public void merge(int[] array, int start, int mid, int end) {
-            int[] temp = new int[end - start]; //temporary merge array
-            int j = 0;
-            int r = 0;
-            int i = 0;
-            
-            while(j < mid - start && r < end - mid) {
-                if(array[start + j] < array[mid + r]) {
-                    temp[i] = array[start + j];
-                    j++;
-                    i++;
-                } else {
-                    temp[i] = array[mid + r];
-                    r++;
-                    i++;
-                }//end if/else
-            }//end while
-            
-            //cleans up remaining items from the right array
-            while(r < end - mid) {
-                temp[i] = array[mid + r];
-                i++;
-                r++;
-            }//end while
-            
-            //Cleans up remaining items from the left array
-            while(j < mid - start) {
+        }
+        return array;
+    }//end sort
+
+    //Merges arrays together in ordered position.
+    public void merge(int[] array, int start, int mid, int end) {
+        int[] temp = new int[end - start]; //temporary merge array
+        int j = 0;
+        int r = 0;
+        int i = 0;
+
+        while(j < mid - start && r < end - mid) {
+            if(array[start + j] < array[mid + r]) {
                 temp[i] = array[start + j];
                 j++;
                 i++;
-            }//end while
-            
-            //Manual Array Copy from *start* to *temp.length*
-            for(i = 0; i < temp.length; i++) {
-                array[start + i] = temp[i];
-            }
-        }//end merge
+            } else {
+                temp[i] = array[mid + r];
+                r++;
+                i++;
+            }//end if/else
+        }//end while
+
+        //cleans up remaining items from the right array
+        while(r < end - mid) {
+            temp[i] = array[mid + r];
+            i++;
+            r++;
+        }//end while
+
+        //Cleans up remaining items from the left array
+        while(j < mid - start) {
+            temp[i] = array[start + j];
+            j++;
+            i++;
+        }//end while
+
+        //Manual Array Copy from *start* to *temp.length*
+        for(i = 0; i < temp.length; i++) {
+            array[start + i] = temp[i];
+        }
+    }//end merge
 }
--- Lab10/src/Quicksort.java
+++ Lab10/src/Quicksort.java
@@ -7,43 +7,43 @@ import java.util.Random;
  * @author: Conor J. Mahoney
  */
 public class Quicksort {
-	private Random randy = new Random();
-	
-        //Breaks the array into partitions, and sorts the partitions
-	private int partition(int[] array, int min, int max) {
-		int pivotIndex = randy.nextInt(max - min + 1) + min; //random pivot each time
-		int pivot = array[pivotIndex];
-		int a = min;
-                int b = max;
-		
-		while(a <= b) { //while the iterating and decrementing values are not equal to each other
-                    while(array[a] < pivot) { //if the item at array[a] is less than the pivot, increment a
-                        a++;
-                    }
-                    while(array[b] > pivot) { //if the item at array[b] is greater than the pivot, increment b
-                        b--;
-                    }
-                    if(a <= b) {
-                        //swap array[a] and array[b]
-                        int temp = array[a];
-                        array[a] = array[b];
-                        array[b] = temp;
-                        a++; //increment a
-                        b--; //decrement b
-                    }
-                }		
-		return a; //return a as the new index
-	}//end partition
-	
-	public int[] sort(int[] array, int min, int max) {
-            int index = partition(array, min, max);
-            
-            if(min < index - 1) { //If the minimum is below index - 1, send the array to be partitioned and sorted again
-                sort(array, min, index - 1);
+    private Random randy = new Random();
+
+    //Breaks the array into partitions, and sorts the partitions
+    private int partition(int[] array, int min, int max) {
+        int pivotIndex = randy.nextInt(max - min + 1) + min; //random pivot each time
+        int pivot = array[pivotIndex];
+        int a = min;
+        int b = max;
+
+        while(a <= b) { //while the iterating and decrementing values are not equal to each other
+            while(array[a] < pivot) { //if the item at array[a] is less than the pivot, increment a
+                a++;
             }
-            if(index < max) { //If the maximum is greater than the index, send the array to be sorted again.
-                sort(array, index, max);
+            while(array[b] > pivot) { //if the item at array[b] is greater than the pivot, increment b
+                b--;
             }
-            return array;
-	}//end sort
+            if(a <= b) {
+                //swap array[a] and array[b]
+                int temp = array[a];
+                array[a] = array[b];
+                array[b] = temp;
+                a++; //increment a
+                b--; //decrement b
+            }
+        }
+        return a; //return a as the new index
+    }//end partition
+
+    public int[] sort(int[] array, int min, int max) {
+        int index = partition(array, min, max);
+
+        if(min < index - 1) { //If the minimum is below index - 1, send the array to be partitioned and sorted again
+            sort(array, min, index - 1);
+        }
+        if(index < max) { //If the maximum is greater than the index, send the array to be sorted again.
+            sort(array, index, max);
+        }
+        return array;
+    }//end sort
 }
